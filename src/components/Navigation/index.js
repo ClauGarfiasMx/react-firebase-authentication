48bb7b02cd05790fd7c0a04aafc2b980ffd9595b
ClaudiaGarfias <me@claudiagarfias.works>
2019-05-09 12:23:40 -0500

    ✔️ React Router added. Working on route to component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,29 @@
-import React from "react";
-const Navigation = () => (
-  <div>
-    {" "}
-    <h1> Navigation </h1>{" "}
-  </div>
-);
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
+const Navigation = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={ROUTES.SIGN_IN}>Entrar</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.LANDING}>¡Bienvenido!</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.HOME}>Home</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.ACCOUNT}>Cuenta</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.ADMIN}>Administración</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 export default Navigation;
