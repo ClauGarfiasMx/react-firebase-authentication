a07a0723d32cb2e912a9ca4ad8637cf4dbe006d8
ClaudiaGarfias <me@claudiagarfias.works>
2019-05-10 12:44:06 -0500

    A BUTTON ADDED in Button Workshop with @juanjordan

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React from 'react';
+import Button from '../Button';
 const Landing = () => (
   <div>
-    {" "}
-    <h1> Landing </h1>{" "}
+    {' '}
+    <h1> Landing </h1>{' '}
+    <Button
+      name="A Button"
+      action={() => {
+        console.log('a button');
+        window.location.href = 'https://google.com';
+      }}
+    />
+    <Button
+      name="Another Button"
+      action={() => {
+        console.log('another button');
+      }}
+      extraClassName="shadow"
+    />
   </div>
 );
+
 export default Landing;
