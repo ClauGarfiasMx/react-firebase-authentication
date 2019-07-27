db3a5fffcefd234e32163ebe8073851671477fc3
ClauGarfiasMx <apps@claudiagarfias.works>
2019-07-27 11:37:04 -0500

    Trying deployment on my hosting, it worked, yay

---
+++ src/components/Home/Try.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import withFirebase from '../Firebase';
+
+// class Try extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//   render() {
+//     return <div>Try</div>;
+//   }
+// }
+
+// export default withFirebase(Try);
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Try from './Try';
 const Home = () => (
   <div>
-    <h1> Home </h1>
+    <h1> Home... changing things just a little bit! </h1>
     <p>Currently using React {React.version}</p>
-    <Try />
+    {/* <Try /> */}
   </div>
 );
 
